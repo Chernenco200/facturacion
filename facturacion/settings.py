c3d6342f72b88a688e2cef4603dd8bf6986fc7d6
Ruidias Román <jruidias@mef.gob.pe>
2026-02-20 21:20:41 -0500

    setting5

--- facturacion/settings.py
+++ facturacion/settings.py
@@ -108,6 +108,7 @@ CLOUDINARY_STORAGE = {
     "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
     "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
     "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+    "FOLDER": "optica_ic",
 }
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
