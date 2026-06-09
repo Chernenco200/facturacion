43e8e7b9e0e40510bd54cd20e44c611404e24220
Ruidias Román <jruidias@mef.gob.pe>
2026-06-08 22:38:17 -0500

    configurar whasapp2

--- whatsapp/views.py
+++ whatsapp/views.py
@@ -18,13 +18,13 @@ def responder_mensaje(numero, texto):
 
     if texto in ["hola", "buenas", "menu", "menú"]:
         mensaje = (
-            "Hola, somos Óptica IC 👓\n\n"
+            "Hola 👋 Bienvenido a Óptica IC.\n\n"
             "Elige una opción:\n\n"
-            "1. Horario de atención\n"
-            "2. Estado de mi ticket\n"
-            "3. Ubicación\n"
-            "4. Sacar una cita\n"
-            "5. Hablar con un asesor"
+            "1️⃣ Horario de atención\n"
+            "2️⃣ Estado de mi ticket\n"
+            "3️⃣ Ubicación\n"
+            "4️⃣ Sacar una cita\n"
+            "5️⃣ Hablar con un asesor"
         )
         enviar_whatsapp_texto(numero, mensaje)
         return
@@ -32,15 +32,16 @@ def responder_mensaje(numero, texto):
     if texto == "1" or "horario" in texto:
         enviar_whatsapp_texto(
             numero,
-            "Nuestro horario de atención es de lunes a sábado de 9:00 a.m. a 8:00 p.m."
+            "Nuestro horario de atención es de lunes a sábado de 9:00 a.m. a 7:45 p.m."
+            "y domingos y feriados de 10:30 am a 6:30 pm"
         )
         return
 
     if texto == "3" or "ubicacion" in texto or "ubicación" in texto:
         enviar_whatsapp_texto(
             numero,
-            "Estamos ubicados en: COLOCA AQUÍ TU DIRECCIÓN.\n\n"
-            "Google Maps: COLOCA AQUÍ TU LINK."
+            "Estamos ubicados en: Jr. Camaná 560 - Cercado de Lima\n\n"
+            "Google Maps: https://www.google.com/maps/dir//%C3%93ptica+IC,+Jr.+Caman%C3%A1+560,+Lima+15001/@-12.0478674,-77.0366906,17z/data=!4m16!1m7!3m6!1s0x9105c8c9f0be65c7:0xa08a1286e936afe0!2s%C3%93ptica+IC!8m2!3d-12.0478674!4d-77.0341157!16s%2Fg%2F11b6mpxj6x!4m7!1m0!1m5!1m1!1s0x9105c8c9f0be65c7:0xa08a1286e936afe0!2m2!1d-77.0341157!2d-12.0478674?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D"
         )
         return
 
@@ -66,8 +67,8 @@ def responder_mensaje(numero, texto):
         enviar_whatsapp_texto(
             numero,
             "Para consultar tu ticket escribe:\n\n"
-            "TICKET 000123\n\n"
-            "Ejemplo: TICKET 000045"
+            "T000123\n\n"
+            "Ejemplo: T00045"
         )
         return
 
